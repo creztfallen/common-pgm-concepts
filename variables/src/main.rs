e44b8d04cfe2d0162170f5cf4a67034524dd3eb0
Mateus <creztfallen@M.S.M>
2022-06-16 21:44:40 -0300

    testing mutability

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,9 @@ fn main() {
     println!("The value of x is {}", x);
     x = 6;
     println!("The value of x is {}", x);
+
+    let y = 5;
+    let y = y + 5;
+    let y = y * 10;
+    println!("The value of y is {}", y);
 }
