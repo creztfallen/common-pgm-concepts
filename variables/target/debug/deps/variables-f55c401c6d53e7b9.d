7e453ac75852f8692d93596416860f22313a4af7
Mateus <creztfallen@M.S.M>
2022-06-16 21:41:36 -0300

    review basic concepts with rust

---
+++ variables/target/debug/deps/variables-f55c401c6d53e7b9.d
@@ -0,0 +1,5 @@
+/home/creztfallen/Documentos/develop/common-pgm-concepts/variables/target/debug/deps/variables-f55c401c6d53e7b9: src/main.rs
+
+/home/creztfallen/Documentos/develop/common-pgm-concepts/variables/target/debug/deps/variables-f55c401c6d53e7b9.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/creztfallen/Documentos/develop/common-pgm-concepts/variables/target/debug/variables: /home/creztfallen/Documentos/develop/common-pgm-concepts/variables/src/main.rs
