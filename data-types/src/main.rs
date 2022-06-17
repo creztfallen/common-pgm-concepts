6c460a5599c41503eafc3aaaad6017445934ed2c
Mateus <creztfallen@M.S.M>
2022-06-17 12:05:32 -0300

    checking more data types

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -1,6 +1,65 @@
 fn main() {
-    let x: i8 = -50;
-    let y: u8 = 255;
+    //NUMBER TYPE
 
-    println!("{}, {}", x, y);
+    //integers--------------------------------------------//
+    let _x: i8 = -50;
+    let _y: u8 = 255;
+
+    //floating-point types--------------------------------//
+    let _a = 2.7; //f64 by default
+    let _b: f32 = 2.0;
+
+    //math operations ------------------------------------//
+
+    // addition
+    let _sum = 5 + 10;
+
+    // subtraction
+    let _difference = 95.5 - 4.3;
+
+    // multiplication
+    let _product = 4 * 30;
+
+    // division
+    let _quotient = 56.7 / 32.2;
+
+    // remainder
+    let _remainder = 43 % 5;
+
+    //boolean---------------------------------------------//
+
+    let _t = true;
+    let _f: bool = false; // with explicit type annotation
+
+    //CHARACTER TYPE
+
+    let _c = 'z';
+    let _z = 'Z';
+
+    //tuple type
+    let _tup: (i32, f64, u8) = (500, 6.4, 1); //the annotations are optionals
+
+    let (_x2, _y2, _z2) = _tup;
+
+    let five_hundred = _tup.0;
+    println!("{}", five_hundred);
+
+    //array type
+    let _months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    let _first = _months[0];
+    let _second = _months[1];
 }
---
+++ data-types/target/debug/deps/data_types-cfe5ccfc5276183a.d
@@ -0,0 +1,5 @@
+/home/creztfallen/Documentos/develop/common-pgm-concepts/data-types/target/debug/deps/data_types-cfe5ccfc5276183a.rmeta: src/main.rs
+
+/home/creztfallen/Documentos/develop/common-pgm-concepts/data-types/target/debug/deps/data_types-cfe5ccfc5276183a.d: src/main.rs
+
+src/main.rs:
